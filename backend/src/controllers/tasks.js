82f17ef7ee8a7350f2c600a468cb83d8e85f6789
Moeed Sarwar <moeedsarwar112@gmail.com>
2025-09-20 05:30:50 +0500

    Revert "fixed backend for sockets"
    This reverts commit e16890f87cdb692240fe8eece9d51e17ead1fd78.

--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -1,6 +1,5 @@
 const Task = require("../models/taskSchema");
 const mongoose = require("mongoose");
-const { getIo, onlineUsers } = require("./socket");
 
 const fetchAllTasks = async (req, res) => {
   try {
@@ -61,18 +60,6 @@ const addTask = async (req, res) => {
       assignedTo,
     });
     await newTask.save();
-
-    // Socket.io event
-    const io = getIo();
-    if (assignedTo && assignedTo.length > 0) {
-      assignedTo.forEach((userId) => {
-        const socketId = onlineUsers.get(userId);
-        if (socketId) {
-          io.to(socketId).emit("task:new", newTask);
-        }
-      });
-    }
-
     res.status(201).json(newTask);
   } catch (error) {
     console.error("Error adding task:", error);
@@ -101,16 +88,6 @@ const deleteTask = async (req, res) => {
       });
     }
 
-    // Socket.io event
-    const io = getIo();
-    const recipients = [task.user.toString(), ...task.assignedTo.map(id => id.toString())];
-    recipients.forEach((recipientId) => {
-        const socketId = onlineUsers.get(recipientId);
-        if (socketId) {
-            io.to(socketId).emit("task:delete", taskID);
-        }
-    });
-
     res.status(204).send();
   } catch (error) {
     console.error("Error deleting task:", error);
@@ -135,17 +112,6 @@ const updateTask = async (req, res) => {
         .status(404)
         .json({ error: "Task not found or not authorized" });
     }
-
-    // Socket.io event
-    const io = getIo();
-    const recipients = [updatedTask.user.toString(), ...updatedTask.assignedTo.map(id => id.toString())];
-    recipients.forEach((recipientId) => {
-        const socketId = onlineUsers.get(recipientId);
-        if (socketId) {
-            io.to(socketId).emit("task:update", updatedTask);
-        }
-    });
-
 
     res.json(updatedTask);
   } catch (error) {
